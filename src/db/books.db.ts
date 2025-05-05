1de8d743d85a12e4d50e2b9856f87c025d8af2cb
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-05-05 15:55:35 +0100

    go

--- src/db/books.db.ts
+++ src/db/books.db.ts
@@ -1,27 +1,26 @@
-import BooksAttributeI from "../interfaces/books.interfaces";
+import BooksAttributeI from '../interfaces/books.interfaces';
 
-const Books: BooksAttributeI[]  = 
-[
-    {
-        id: 1,
-        name: 'Think Big',
-        author: 'Ben Carson',
-        pages: 345,
-    },
+const Books: BooksAttributeI[] = [
+  {
+    id: 1,
+    name: 'Think Big',
+    author: 'Ben Carson',
+    pages: 345
+  },
 
-    {
-        id: 2,
-        name: 'The Broke Millenienial',
-        author: 'Cag Joy',
-        pages: 367,
-    },
+  {
+    id: 2,
+    name: 'The Broke Millenienial',
+    author: 'Cag Joy',
+    pages: 367
+  },
 
-    {
-        id: 3,
-        name: 'Think Big',
-        author: 'Ben Carson',
-        pages: 125,
-    },
-]
+  {
+    id: 3,
+    name: 'Think Big',
+    author: 'Ben Carson',
+    pages: 125
+  }
+];
 
 export default Books;
